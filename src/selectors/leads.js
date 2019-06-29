5311141d62e4cac7e4b22fe619fa9f15f4cb38f2
Angelo Arboscello <angeloarboscello@Angelos-MBP.fritz.box>
2019-06-29 15:17:05 +0200

    Aggiunto sort alle richieste

--- src/selectors/leads.js
+++ src/selectors/leads.js
@@ -6,6 +6,8 @@ export default (leads, { lead, leadStato }) => {
         const budgetMatch = illead.leadBudget >= (lead * 100)
         const statoMatch = illead.leadOggettoStato === leadStato || leadStato === '' || illead.leadOggettoStato === ''
     return budgetMatch && statoMatch
+    }).sort((a, b) => {
+        return a.leadBudget > b.leadBudget ? 1 : -1
     })
 
 }
