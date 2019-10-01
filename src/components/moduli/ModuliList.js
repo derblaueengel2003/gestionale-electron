15db66254cab9066a1aa6b267f11d207020d733f
Angelo Arboscello <angeloarboscello@imac-angelo.localdomain>
2019-10-01 13:58:46 +0200

    Bug fix moduli übergabe

--- src/components/moduli/ModuliList.js
+++ src/components/moduli/ModuliList.js
@@ -160,7 +160,7 @@ const ModuliPage = () => (
         </a>
       </div>
       <div className='list-item'>
-        <a href='./files/Übergabeprotokoll.doc' download>
+        <a href='./files/Übergabeprotokoll.docx' download>
           Übergabe-Protokoll Verkauf
         </a>
       </div>
