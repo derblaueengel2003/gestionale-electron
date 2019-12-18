1b21264d4ac36d36805452e8a6a78a88771db083
Angelo Arboscello <angelo.arboscello@me.com>
2019-12-18 15:47:22 +0100

    boh

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -5,11 +5,8 @@ export const login = uid => ({
   uid
 });
 
-export const startLogin = (email, password) => {
+export const startLogin = () => {
   return () => {
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-
     return firebase
       .auth()
       .signInWithPopup(googleAuthProvider)
@@ -19,18 +16,6 @@ export const startLogin = (email, password) => {
       .catch(err => {
         console.log(err);
       });
-    // return firebase.auth().signInWithPopup(googleAuthProvider)
-    // return firebase.auth().signInWithEmailAndPassword(email, password).catch((error) => {
-    //     const errorCode = error.code;
-    //     const errorMessage = error.message;
-    //     // [START_EXCLUDE]
-    //     if (errorCode === 'auth/wrong-password') {
-    //       alert('Wrong password.');
-    //     } else {
-    //       alert(errorMessage);
-    //     }
-    //     console.log(error);
-    // })
   };
 };
 
