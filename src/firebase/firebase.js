77c68d5edc30749299cf461a6a76e98fbee87f97
Angelo Arboscello <angeloarboscello@Angelos-MacBook-Pro.local>
2019-12-24 17:15:25 +0100

    clienti kundenbetreuer fix

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,17 @@
 import * as firebase from 'firebase';
 import 'firebase/storage';
+import google from 'googleapis';
+
+const oauth2Client = new google.auth.OAuth2(
+  '692290965696-40un6en5v8cof0iennkj1gjkafmoasft.apps.googleusercontent.com',
+  '27dGP7NbGdFSPB5Fd4oaH9jq',
+  'https://gestionale-dummy.firebaseapp.com/__/auth/handler'
+);
+
+// set auth as a global default
+google.options({
+  auth: oauth2Client
+});
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
