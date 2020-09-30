5fb22c22e9e90532aa57efda00d17e993e80f373
Angelo Arboscello <angelo.arboscello@me.com>
2020-09-30 13:26:56 +0200

    nuovo index

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const electron = require('electron');
-const { app, BrowserWindow, ipcMain, shell, Menu } = electron;
+const { app, BrowserWindow, ipcMain, shell } = electron;
 const axios = require('axios');
 const path = require('path');
 const homedir = require('os').homedir();
@@ -7,7 +7,6 @@ const fs = require('fs');
 const FormData = require('form-data');
 
 let mainWindow;
-let addWindow;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
@@ -16,68 +15,12 @@ app.on('ready', () => {
     webPreferences: {
       backgroundThrottling: false,
       nodeIntegration: true,
-      // webSecurity: false,
     },
   });
 
   // carico la pagina
   mainWindow.loadURL(`file://${__dirname}/public/index.html`);
-  // mainWindow.on('closed', () => app.quit());
-
-  // creo il menu
-  // const mainMenu = Menu.buildFromTemplate(menuTemplate);
-  // e lo inserisco
-  // Menu.setApplicationMenu(mainMenu);
 });
-
-// function createAddWindow(event, title) {
-//   addWindow = new BrowserWindow({
-//     height: 800,
-//     width: 600,
-//     title,
-//     webPreferences: {
-//       nodeIntegration: true,
-//     },
-//   });
-//   // addWindow.loadURL(`file://${__dirname}/public/index.html#/clienticreate`);
-//   addWindow.on('close', () => (addWindow = null));
-// }
-
-// Menu
-// const menuTemplate = [
-//   { label: 'Gestionale m2Square' },
-//   {
-//     label: 'File',
-//     submenu: [
-//       // {
-//       //   label: 'Nuovo contatto',
-//       //   click() {
-//       //     createAddWindow();
-//       //   },
-//       // },
-//       { role: 'quit' },
-//     ],
-//   },
-//   {
-//     label: 'Edit',
-//     submenu: [
-//       { role: 'undo' },
-//       { role: 'redo' },
-//       { type: 'separator' },
-//       { role: 'cut' },
-//       { role: 'copy' },
-//       { role: 'paste' },
-//     ],
-//   },
-// ];
-
-// controllo l'environment di node che può essere: production, development, staging, test
-// if (process.env.NODE_ENV !== 'production') {
-//   menuTemplate.push({
-//     label: 'Developer',
-//     submenu: [{ role: 'toggledevtools' }],
-//   });
-// }
 
 ipcMain.on('window:reload', (event) => {
   mainWindow.loadURL(`file://${__dirname}/public/index.html`);
@@ -94,15 +37,6 @@ ipcMain.on('oggetto:fetch', async (event, url) => {
     mainWindow.webContents.send('oggetto:spinner', false);
   } catch (error) {
     mainWindow.webContents.send('oggetto:error', error);
-  }
-});
-
-ipcMain.on('is24:send', async (event, options) => {
-  try {
-    const { data } = await axios(options);
-    mainWindow.webContents.send('is24:response', data);
-  } catch (error) {
-    mainWindow.webContents.send('is24:error', error);
   }
 });
 
@@ -123,6 +57,16 @@ ipcMain.on('folder:open', (event, { folder, folderNamePartial }) => {
 
 ipcMain.on('link:open', (event, link) => {
   shell.openExternal(link);
+});
+
+//////////////////// IS24
+ipcMain.on('is24:send', async (event, options) => {
+  try {
+    const { data } = await axios(options);
+    mainWindow.webContents.send('is24:response', data);
+  } catch (error) {
+    mainWindow.webContents.send('is24:error', error);
+  }
 });
 
 ipcMain.on('is24img:upload', (event, options) => {
