a7b213be692d85a24d8a6ca782be6b7b55333831
Angelo Arboscello <angelo.arboscello@me.com>
2020-10-08 13:22:34 +0200

    downgrade electron per poter usare redux devtools

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const electron = require('electron');
 const { app, BrowserWindow, ipcMain, shell } = electron;
+const {
+  default: installExtension,
+  REDUX_DEVTOOLS,
+} = require('electron-devtools-installer');
+
 const axios = require('axios');
 const path = require('path');
 const homedir = require('os').homedir();
@@ -7,6 +12,12 @@ const fs = require('fs');
 const FormData = require('form-data');
 
 let mainWindow;
+
+app.whenReady().then(() => {
+  installExtension(REDUX_DEVTOOLS)
+    .then((name) => console.log(`Added Extension:  ${name}`))
+    .catch((err) => console.log('An error occurred: ', err));
+});
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
